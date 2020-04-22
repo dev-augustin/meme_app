fa17f2f1b164779fdd1ec9ba05074849a2b942db
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 12:57:47 -0500

    updated pseudocode

--- meme_app/pseudocode/pseudocode.js
+++ meme_app/pseudocode/pseudocode.js
@@ -3,3 +3,10 @@
 // Create a wireframe
 // Create README file with problem statement and required details
 // Clean up REACT App.js and App.css
+
+// Create Component files for your project under src
+// Create Routes for Search and CreateMeme components files
+// In CreateMeme file, create form  input fiels to create meme
+// In search file, make Axios request to fetch memes
+// In SearchResutls file, display the results file passed as props from Search file
+//
