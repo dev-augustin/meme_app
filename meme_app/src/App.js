cecfda293777f7865fc3199ba3758c4c96b39042
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 12:07:43 -0500

    added initial code

--- meme_app/src/App.js
+++ meme_app/src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
-      
+      <h1>Welcome to Meme Generator</h1>
     </div>
+    <div>
+      <button>Search</button>
+      <button>Create Meme</button>
+    </div>
+    </React.Fragment>
   );
 }
 
