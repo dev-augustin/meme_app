fe2c78d2d5849f6c788c8555abc6aa286e07464e
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 12:23:33 -0500

    added Home.js

---
+++ meme_app/src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+import Search from "./Search";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+export default function Home() {
+    return (
+        <div>
+        <Router>
+        
+        </Router>
+        </div>
+    //     <div>
+    //         <React.Fragment>
+    // <div className="App">
+    //   <h1>Welcome to Meme Generator</h1>
+    // </div>
+    // <div>
+    //   <button onClick={<Search/>}>Search</button>
+    //   <button>Create Meme</button>
+    // </div>
+    // </React.Fragment>
+    //     </div>
+
+
+    )
+}
