d253d7bb22a0020c78f44a510ec24072a10b28c0
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 16:52:27 -0500

    changed stateful component

--- meme_app/src/components/Search.js
+++ meme_app/src/components/Search.js
@@ -1,9 +1,36 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Search() {
-    return (
-        <div>
-            Hi
-        </div>
-    )
+// export default function Search() {
+//     return (
+//         <div>
+//             <form>
+                
+//                 <input type="text" name="Search" value="Search" placeholder="Search"/>
+//             <input type="Submit" name="Search" value="Search" />
+//             </form>
+//         </div>
+//     )
+// }
+// This will be a stateful component 
+import React, { Component } from 'react';
+import axios from "axios";
+
+export default class Search extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchValue
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="Search" value="Search" placeholder="Search"/>
+                    <input type="Submit" name="Search" value="Search" />
+                 </form>
+            </div>
+        )
+    }
 }
+
