60844df1eecf6da8022836b8fd3f8df43e4556d1
dev-augustin <dev.augustin00@gmail.com>
2020-04-22 10:50:36 -0500

    initial pseudocode

---
+++ meme_app/pseudocode/pseudocode.js
@@ -0,0 +1,5 @@
+// Create github repo for the Project
+// Clone it and create react app
+// Create a wireframe
+// Create README file with problem statement and required details
+// Clean up REACT App.js and App.css
