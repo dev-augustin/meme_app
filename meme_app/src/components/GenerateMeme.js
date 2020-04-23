a17171b0403ef72a82585d88669b2bd7261aa18b
dev-augustin <dev.augustin00@gmail.com>
2020-04-23 01:28:45 -0500

    update file name

---
+++ meme_app/src/components/GenerateMeme.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+const userName=process.env.REACT_APP_USERNAME;
+const password=process.env.REACT_APP_PASSWORD;
+console.log(userName)
+export default class GenerateMeme extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
