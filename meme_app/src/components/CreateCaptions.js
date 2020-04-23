bfbe4462eb323a98818d500bc5b6902c55b45192
dev-augustin <dev.augustin00@gmail.com>
2020-04-23 12:04:55 -0500

    updated onSumbit and created POST request

--- meme_app/src/components/CreateCaptions.js
+++ meme_app/src/components/CreateCaptions.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import HowTo from "./HowTo"
-import axios from 'axios';
+import "How"
 //import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
@@ -18,49 +17,31 @@ export default class GenerateMeme extends Component {
             text1: ""
         }
     }
-    
-    handleChange = (event) => {
-       event.preventDefault();
-       console.log(event.target.value)
-       this.setState({[event.target.name] :event.target.value})
-    }
-    
-    onSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state.username)
-        let memeData={
-            template_id : this.state.template_id,
-            username : this.state.username,
-            password : this.state.password,
-            text0: this.state.picture,
-            text1: this.state.text1
-        };
-        console.log("memeData: ", memeData);
-        console.log("username: ", username);
-        this.postAPI(memeData);
-    }
 
-    async postAPI(memeData){
-        console.log("PostAPI: ", memeData);
-        const response = await axios.post("", memeData)
+    handleChange(){
+
+    }
+    
+    onSubmit(){
+
     }
     render() {
         return (
             <div>
                <form>
-                  <label>User Name</label>
+                  <label>username</label>
                   <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
-                  <label>Password</label>
+                  <label>username</label>
                   <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                  <label>Template_ID</label>
+                  <label>template_id</label>
                   <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/>
-                  <label>Text0</label>
+                  <label>username</label>
                   <input type = "text" name="text0" value={this.state.text0} onChange={this.handleChange}/>
-                  <label>Text1</label>
+                  <label>username</label>
                   <input type = "text" name="text1" value={this.state.text1} onChange={this.handleChange}/>
                   <button onClick={this.onSubmit}>Submit</button>
                 </form> 
-               <HowTo/>
+               
             </div>
         )
     }
