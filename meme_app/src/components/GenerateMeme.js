e9082f278883d295fd1ca81ed07e5974a1505b74
dev-augustin <dev.augustin00@gmail.com>
2020-04-23 10:04:42 -0500

    only basic code

--- meme_app/src/components/GenerateMeme.js
+++ meme_app/src/components/GenerateMeme.js
@@ -10,9 +10,6 @@ export default class GenerateMeme extends Component {
     constructor(props){
         super(props);
         this.state={
-            username: " ",
-            password: "",
-            template_id:"",
             text0:"",
             text1: ""
         }
@@ -30,12 +27,12 @@ export default class GenerateMeme extends Component {
         return (
             <div>
                <form>
-                  <label>User Name</label>
+                  {/* <label>User Name</label>
                   <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
                   <label>Password</label>
                   <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
                   <label>Template_ID</label>
-                  <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/>
+                  <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/> */}
                   <label>Text0</label>
                   <input type = "text" name="text0" value={this.state.text0} onChange={this.handleChange}/>
                   <label>Text1</label>
