4beab685135af164fa86ef9f4335a27787f5d99c
dev-augustin <dev.augustin00@gmail.com>
2020-04-23 11:09:32 -0500

    still working on Post request

--- meme_app/src/components/GenerateMeme.js
+++ meme_app/src/components/GenerateMeme.js
@@ -10,6 +10,9 @@ export default class GenerateMeme extends Component {
     constructor(props){
         super(props);
         this.state={
+            username: " ",
+            password: "",
+            template_id:"",
             text0:"",
             text1: ""
         }
@@ -17,6 +20,7 @@ export default class GenerateMeme extends Component {
 
     handleChange = (event) => {
        event.preventDefault();
+       console.log(event.target.value)
        this.setState({[event.target.name] :event.target.value})
     }
     
@@ -27,12 +31,12 @@ export default class GenerateMeme extends Component {
         return (
             <div>
                <form>
-                  {/* <label>User Name</label>
+                  <label>User Name</label>
                   <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
                   <label>Password</label>
                   <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
                   <label>Template_ID</label>
-                  <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/> */}
+                  <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/>
                   <label>Text0</label>
                   <input type = "text" name="text0" value={this.state.text0} onChange={this.handleChange}/>
                   <label>Text1</label>
