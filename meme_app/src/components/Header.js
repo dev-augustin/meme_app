b438d70bfc5327b7bd0a5d2e579d1d159105457e
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 17:06:04 -0500

    Code works. Styling in progress

--- meme_app/src/components/Header.js
+++ meme_app/src/components/Header.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import { Link } from "react-router-dom"
-// import Home from "./Home";
+import { BrowserRouter as Router, Link, Route , Switch} from "react-router-dom"
+ import Home from "./Home";
+import Search from './Search'
 import '../styles/Header.css'
+import GenerateMeme from './GenerateMeme'
 
 export default function Header() {
 
     return (
         <div>
-             {/* <Link to ='/Home' >PriyaHome</Link> */}
+            <h2>Welcome To Meme </h2>
+       
+            {/* <h2>Welcome to Meme Generator</h2> */}
+             {/* <Link to ='/Home'>PriyaHome</Link> */}
             <ul>
             <li>
-                <Link to ='/Search' >Search</Link>
+                <Link to ='/Search' exact>Search</Link>
             </li>
             <li>
                 <Link to ='/GenerateMeme'> Generator</Link>
             </li>
             </ul>
+        {/* <Switch>
+
+<Route exact path="/Search"  component={Search}/>
+<Route path="/GenerateMeme" component={GenerateMeme}/>
+<Route path="/Home" component={Home} />
+</Switch> */}
+       
         </div>
     )
 }
