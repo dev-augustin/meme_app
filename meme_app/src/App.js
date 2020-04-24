428a66b7f357a3f12b987c29999e118fd951f461
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 13:42:36 -0500

    added reference

--- meme_app/src/App.js
+++ meme_app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './components/Home';
 import {BrowserRouter as Router, Link, Route, Redirect, Switch} from 'react-router-dom'
 import Header from './components/Header'
+import Search from './components/Search';
+import GenerateMeme from './components/GenerateMeme';
 function App() {
   return (
     <React.Fragment>
@@ -11,6 +13,8 @@ function App() {
         <Switch>
           <Route exact path = '/' component={Header} />
           <Route exact path='/Home' component={Home}/>
+          <Route exact path='/Search' component={Search} />
+          <Route exact path='/GenerateMeme' component={GenerateMeme} />
         </Switch>
      </Router>
     </div>
