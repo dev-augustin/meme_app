4a1813d0c4526d8e8432d4d2b6e3aebc17fdcbff
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 13:06:27 -0500

    trying Routing

--- meme_app/src/App.js
+++ meme_app/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
-
-
+import {BrowserRouter as Router, Link, Route, Redirect, Switch} from 'react-router-dom'
+import Header from './components/Header'
 function App() {
   return (
     <React.Fragment>
     <div className="App">
-      <Home/>
+     <Router>
+        <Switch>
+          <Route exact path = '/' component={Header} />
+          <Route exact path='/Home' component={Home}/>
+        </Switch>
+     </Router>
     </div>
     </React.Fragment>
   );
