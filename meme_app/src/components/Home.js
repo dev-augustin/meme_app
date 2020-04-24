4c0cd134e25cafbb396e3f1cce4c1c31ffe7c9bf
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 13:04:11 -0500

    trying Routing

--- meme_app/src/components/Home.js
+++ meme_app/src/components/Home.js
@@ -3,7 +3,7 @@ import '../styles/Home.css';
 import Search from "./Search";
 import GenerateMeme from './GenerateMeme'
 import HowTo from './HowTo'
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 
 export default function Home() {
     return (
@@ -23,10 +23,12 @@ export default function Home() {
                     <li><Link to = '/HowTo'> How To <br/>(Instructions to geneate Meme)</Link></li>
                 </ul>
                 
-               
-                <Route path="/Search" exact component={Search}/>
-                <Route path="/GenerateMeme" component={GenerateMeme}/>
-                <Route path="/HowTo" component={HowTo} />
+               <Switch>
+
+                    <Route exact path="/Search"  component={Search}/>
+                    <Route path="/GenerateMeme" component={GenerateMeme}/>
+                    <Route path="/HowTo" component={HowTo} />
+               </Switch>
                 
              </nav>
             </Router>
