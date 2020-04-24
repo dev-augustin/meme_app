49eea8a46e0add7aa4c4c3df820924003abe2020
dev-augustin <dev.augustin00@gmail.com>
2020-04-23 23:57:21 -0500

    MemeResult

--- meme_app/src/components/MemeResult.js
+++ meme_app/src/components/MemeResult.js
@@ -1,49 +1,9 @@
-import React, { Component } from 'react';
-import "How"
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react'
 
-
-const userName=process.env.REACT_APP_USERNAME;
-const password=process.env.REACT_APP_PASSWORD;
-console.log(userName)
-export default class GenerateMeme extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            username: " ",
-            password: "",
-            template_id:"",
-            text0:"",
-            text1: ""
-        }
-    }
-
-    handleChange(){
-
-    }
-    
-    onSubmit(){
-
-    }
-    render() {
-        return (
-            <div>
-               <form>
-                  <label>username</label>
-                  <input type = "text" name="username" value={this.state.username} onChange={this.handleChange}/>
-                  <label>username</label>
-                  <input type = "password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                  <label>template_id</label>
-                  <input type = "text" name="template_id" value={this.state.template_id} onChange={this.handleChange}/>
-                  <label>username</label>
-                  <input type = "text" name="text0" value={this.state.text0} onChange={this.handleChange}/>
-                  <label>username</label>
-                  <input type = "text" name="text1" value={this.state.text1} onChange={this.handleChange}/>
-                  <button onClick={this.onSubmit}>Submit</button>
-                </form> 
-               
-            </div>
-        )
-    }
+export default function MemeResult(props) {
+    return (
+        <div>
+            <img src={props.url} alt="memes" />
+        </div>
+    )
 }
-
