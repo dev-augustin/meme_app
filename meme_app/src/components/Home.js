913e6839ccf60b00280401af85167dae1e1658fc
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 19:06:15 -0500

    added Home file

--- meme_app/src/components/Home.js
+++ meme_app/src/components/Home.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import '../styles/Home.css';
 import Search from "./Search";
-import GenerateMeme from './GenerateMeme'
-import HowTo from './HowTo'
+import GenerateMeme from './GenerateMeme';
+import HowTo from './HowTo';
+import About from './About';
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 
 export default function Home() {
     return (
 
-        <div>
-    
+        <div className="home-container">
 
        <Router>
        <nav>
-           <ul>
+           <ul className="lists-display">
+           <li>  <Link to = '/'>Home </Link> </li>
                <li>
                   <Link to ='/Search'> Search</Link>
                </li>
                <li>
                    <Link to = '/GenerateMeme'>Create Captions for Meme</Link>
                </li>
-               <li>  <Link to = '/Home'>Home </Link> </li>
-               <li><Link to = '/HowTo'> How To <br/>(Instructions to geneate Meme)</Link></li>
-           </ul>
-           
+               
+               <li><Link to = '/HowTo'> Instructions to generate Meme</Link></li>
+           </ul>   
+        </nav>
           <Switch>
-
+   
                <Route exact path="/Search"  component={Search}/>
                <Route path="/GenerateMeme" component={GenerateMeme}/>
                <Route path="/HowTo" component={HowTo} />
+               <Route path="/" component={About} />  
           </Switch>
-           
-        </nav>
+     
        </Router>
+     
    </div>
    
     )
