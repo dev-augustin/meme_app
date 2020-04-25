26d7f73e09e479af178b08d2e590b34f8da20b8f
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 22:45:41 -0500

    MemeResult.js v1.0

--- meme_app/src/components/MemeResult.js
+++ meme_app/src/components/MemeResult.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/MemeResult.css';
 
 export default function MemeResult(props) {
     return (
-        <div>
-            <img src={props.url} alt="memes" />
+        <div  className="captioned-image">
+            <img src={props.url} alt="memes" height="400px" width="400px"/>
         </div>
     )
 }
