9c445bc357754931bab7877cd3153082da99635c
dev-augustin <dev.augustin00@gmail.com>
2020-04-25 03:09:38 -0500

    MememError.js

--- meme_app/src/components/MemeResult.js
+++ meme_app/src/components/MemeResult.js
@@ -4,7 +4,7 @@ import '../styles/MemeResult.css';
 export default function MemeResult(props) {
     return (
         <div  className="captioned-image">
-            <img src={props.url} alt="memes" height="400px" width="400px"/>
+            <img src={props.url} alt="memes" height="300px" width="300px"/>
         </div>
     )
 }
