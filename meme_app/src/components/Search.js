a7e62f489d4b62a23b9518ffea1692e13453a185
dev-augustin <dev.augustin00@gmail.com>
2020-04-25 03:09:59 -0500

    Search.s

--- meme_app/src/components/Search.js
+++ meme_app/src/components/Search.js
@@ -24,18 +24,18 @@ export default class Search extends Component {
             try{
 
                 const submitValue = this.state.searchValue;
-                 console.log("Hello", submitValue)
+                //  console.log("Hello", submitValue)
                 const response = await axios.get("https://cors-anywhere.herokuapp.com/https://api.imgflip.com/get_memes/");
-                console.log("responsedaata : ", response.data)
+                // console.log("responsedaata : ", response.data)
                 // console.log("response.data.data: ", response.data.data);
                 //console.log(response.data.data.memes)
                  const results=response.data.data.memes;
                 //  console.log("Results: ", results); //displays all memes
-                console.log(this.state.searchValue)
+                // console.log(this.state.searchValue)
                  const searchRequest = results.filter(memeFilter => memeFilter.name.toLowerCase().includes(this.state.searchValue));
                 //  console.log(searchRequest);
-                 const fitlerResult=searchRequest.map((memeResult) => (<div>{memeResult.resultName}{memeResult.resultUrl}</div>))
-                 console.log(fitlerResult)
+                //  const fitlerResult=searchRequest.map((memeResult) => (<div>{memeResult.resultName}{memeResult.resultUrl}</div>))
+                //  console.log(fitlerResult)
                 if(searchRequest.length>0){
                  this.setState({searchResult :searchRequest});
                  this.setState({noData: false})
@@ -47,21 +47,21 @@ export default class Search extends Component {
             }
             catch(e){
                 const error= e;
-                console.log("ErrorCatch", error.response);
+                // console.log("ErrorCatch", error.response);
             }
      }
 
     //receiving the value entered in text field to search
     handleChange = (event) =>{
         event.preventDefault();
-        console.log(event.target.value)
+        // console.log(event.target.value)
         this.setState({searchValue :event.target.value})
     }
      
     // function to handle search
     onSearch = (event) =>{
         event.preventDefault();
-        console.log(this.state.searchValue);
+        // console.log(this.state.searchValue);
         this.searchRequest();
     }
     render() {
