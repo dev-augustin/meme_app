d22e346acc949c7e8fd4d8e8cd0c471f25318bd2
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 22:45:04 -0500

    SearchResult.js v1.0

--- meme_app/src/components/SearchResult.js
+++ meme_app/src/components/SearchResult.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 export default function searchResult(props) {
     return (
-        <div>
+        <div className="search-result-container">
            
-                    <div> 
-                        <h1>{props.name}</h1>
-                     <img src={props.url} alt="memes"/> </div>
+                    <div className = "meme-search-result"> 
+                     <h1> {props.name}</h1>
+                     <img src={props.url} alt="memes" height="300px" width="300px"/>          
+                    </div>
                     
         </div>
     )
