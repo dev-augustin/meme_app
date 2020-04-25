9377e8a27c06715bb2a5ac43a7e5919955399793
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 19:05:36 -0500

    HowTo added

--- meme_app/src/components/HowTo.js
+++ meme_app/src/components/HowTo.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../styles/HowTo.css'
 
 export default function HowTo() {
     return (
-        <div>
-            Lorem20sddfgdfbcbcvb cv cv c
-        </div>
+        <React.Fragment>
+            <div className="how-to">
+             <p><h3>Input Parameters</h3>
+                <strong>template_id:</strong> returned by the get_memes response <br/>
+                <strong> username, password:</strong> username of a valid imgflip account. <br/>for <strong>testing</strong> purpose, username: imgflip_hubot  <br/>
+                <strong>text0:</strong> Top text for the meme &nbsp;&nbsp;
+                <strong> text1:</strong> Top text for the meme 
+             </p>
+            </div>
+            <div className="important-note">
+             <p><h3>IMPORTANT NOTE: </h3>
+                If you plan to use the API for anything other than testing, you should create your own Imgflip account and use that instead of the default imgflip_hubot account, because that account may be limited as soon as you send a non-trivial number of requests, and then your script wouldn't be able to make memes anymore, and everyone would be sad.
+             </p>
+             <p><strong>Source: for further instructions</strong>https://api.imgflip.com </p>
+             <p><a href="https://api.imgflip.com/popular_meme_ids" alt="template-ids"> Link :template_id for meme images</a></p>
+            </div>
+        </React.Fragment>
+       
     )
 }
 
