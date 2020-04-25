344b0d41e7a59bf5b64e6436e8c894c3e727af93
dev-augustin <dev.augustin00@gmail.com>
2020-04-25 12:35:06 -0500

    MemeError.js file added. Error displayed

--- meme_app/src/components/MemeError.js
+++ meme_app/src/components/MemeError.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/MemeError.css'
 
 export default function erro(props) {
     return (
-        <div>
-          <h1>{props.message}</h1>
+        <div >
+          <h1 className="meme-error-message">{props.message}</h1>
         </div>
     )
 }
