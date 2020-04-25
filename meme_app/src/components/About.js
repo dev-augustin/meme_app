f6fb416d87d9113558a4ab70000e07cdd7dad193
dev-augustin <dev.augustin00@gmail.com>
2020-04-24 19:03:37 -0500

    added About

---
+++ meme_app/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/About.css';
+
+export default function About() {
+    return (
+        <div className="about-meme-app">
+            <p>Meme Generator App is built using Img API.</p> 
+            <p>The Imgflip API uses a RESTful JSON interface</p>
+            <h3>get_memes</h3>    
+            <p>URL: https://api.imgflip.com/get_memes &nbsp;&nbsp; Method: GET</p>
+            <h3>caption_img</h3>
+            <p>URL:https://api.imgflip.com/caption_image &nbsp;&nbsp; Method: POST</p>
+            <p><strong>Source:</strong>https://api.imgflip.com </p>
+        </div>
+    )
+}
